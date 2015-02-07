bcf8f2a3482d00d932ca3f2d0a560990aa2d1cab
GNU <BaconBurgerPizza@gmail.com>
2015-02-07 18:27:42 -0500

    Test
    just a temp thing to see if egit is connected to Github

---
+++ Graph/src/test.java
@@ -0,0 +1,8 @@
+public class test {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a test to see that egit is properly setup to work with github");
+	}
+	
+}
