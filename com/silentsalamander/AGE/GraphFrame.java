6a6a80cb08dc40854e697caff12736c12dce72f5
Ivan Johnson <baconburgerpizza@gmail.com>
2016-04-09 20:11:53 -0500

    Complete rewrite of AGE's GUI.
    Can now graph multiple equations at once, and one equation can call another!

---
+++ com/silentsalamander/AGE/GraphFrame.java
@@ -0,0 +1,286 @@
+// This file is part of AGE
+//
+// AGE Graphs Equations (AGE) is a java program that graphs equations
+// Copyright (C) 2016 Ivan Johnson:
+// fire.4@cox.net
+//
+// AGE is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+package com.silentsalamander.AGE;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
+import com.silentsalamander.helper.NumberTextField;
+import com.silentsalamander.helper.PrettyLogger;
+import com.silentsalamander.helper.equation.Equation;
+
+public class GraphFrame extends JFrame {
+  // TODO
+  
+  // fix the current issue with duplicate names after removing out of order...
+  
+  // allow renaming equations. This will involve checking for duplicate names on name change and
+  // equation add. Will probably be forced to leave missing names if removing out of order.
+  
+  // allow one equation to call another
+  
+  // options. esp. radian or degree mode.
+  
+  // graph equations in different colors (maybe option to display a drop-down selection for each
+  // equation?)
+  
+  protected static PrettyLogger log;
+  private static final long     serialVersionUID = 2266856448062439731L;
+  static {
+    log = PrettyLogger.getPrimaryLogger();
+  }
+  
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        JFrame f = new GraphFrame();
+        f.pack();
+        f.setVisible(true);
+        f.setLocationRelativeTo(null);
+      }
+    });
+  }
+  
+  protected ArrayList<Equation>   equationEquation;
+  protected ArrayList<JLabel>     equationName;
+  protected ArrayList<JTextField> equationText;
+  protected JLabel                labelXMin, labelXMax, labelYMin, labelYMax;
+  protected GraphPanel            panelGraph;
+  protected JPanel                panelMaster;
+  protected JTabbedPane           panelTop;
+  protected JPanel                panelTopEquation;
+  protected JPanel                panelTopWindow;
+  protected NumberTextField       textXMin, textXMax, textYMin, textYMax;
+  
+  public GraphFrame() {
+    // #####CREATES THE TOP PANEL
+    // equation panel
+    equationText = new ArrayList<>();
+    equationName = new ArrayList<>();
+    equationEquation = new ArrayList<>();
+    addEquation();
+    
+    // window panel
+    textXMin = new NumberTextField(-10);
+    textXMax = new NumberTextField(10);
+    textYMin = new NumberTextField(-10);
+    textYMax = new NumberTextField(10);
+    labelXMin = new JLabel("X-Min");
+    labelXMin.setHorizontalAlignment(SwingConstants.CENTER);
+    labelXMax = new JLabel("X-Max");
+    labelXMax.setHorizontalAlignment(SwingConstants.CENTER);
+    labelYMin = new JLabel("Y-Min");
+    labelYMin.setHorizontalAlignment(SwingConstants.CENTER);
+    labelYMax = new JLabel("Y-Max");
+    labelYMax.setHorizontalAlignment(SwingConstants.CENTER);
+    panelTopWindow = new JPanel();
+    panelTopWindow.setLayout(new GridLayout(2, 5));
+    panelTopWindow.add(labelXMin);
+    panelTopWindow.add(labelXMax);
+    panelTopWindow.add(labelYMin);
+    panelTopWindow.add(labelYMax);
+    panelTopWindow.add(textXMin);
+    panelTopWindow.add(textXMax);
+    panelTopWindow.add(textYMin);
+    panelTopWindow.add(textYMax);
+    
+    
+    panelTop = new JTabbedPane();
+    panelTop.addTab("Equations", panelTopEquation);
+    panelTop.addTab("Window", panelTopWindow);
+    panelTop.setPreferredSize(new Dimension(500, 100));
+    
+    
+    // #####CREATES THE GRAPH PANEL
+    panelGraph = new GraphPanel(this);
+    panelGraph.setXMin(textXMin.getValueDouble());
+    panelGraph.setXMax(textXMax.getValueDouble());
+    panelGraph.setYMin(textYMin.getValueDouble());
+    panelGraph.setYMax(textYMax.getValueDouble());
+    panelGraph.setPreferredSize(new Dimension(500, 500));
+    
+    
+    // #####CREATES THE MASTER PANEL
+    panelMaster = new JPanel();
+    panelMaster.setLayout(new BorderLayout());
+    panelMaster.add(panelGraph, BorderLayout.CENTER);
+    panelMaster.add(panelTop, BorderLayout.NORTH);
+    
+    // #####duh
+    setContentPane(panelMaster);
+  }
+  
+  protected void addEquation() {
+    equationText.add(new JTextField(30));
+    if (equationName.size() < equationText.size()) {
+      equationName.add(new JLabel(((char) ('a' - 1 + equationText.size())) + "(x)"));
+      equationName.get(equationName.size() - 1).setHorizontalAlignment(SwingConstants.CENTER);
+    }
+    rebuildTabEquation();
+  }
+  
+  public Equation[] getEquation() {
+    if (equationEquation == null || equationEquation.size() == 0) {
+      return null;
+    }
+    Equation[] arr = new Equation[equationEquation.size()];
+    return equationEquation.toArray(arr);
+  }
+  
+  protected void rebuildEquations() {
+    String[] arrX = { "x" };
+    String text, eqName;
+    
+    
+    // Only in unusual circumstances will this do stuff, and when it does it doesn't do much.
+    // just here for good practice.
+    equationEquation.ensureCapacity(equationText.size());
+    
+    
+    while (equationEquation.size() < equationText.size()) {
+      equationEquation.add(null);
+    }
+    
+    for (int x = 0; x < equationText.size(); x++) {
+      text = equationText.get(x).getText().trim();
+      eqName = equationName.get(x).getText().trim();
+      eqName = eqName.substring(0, eqName.length() - 3);
+      if (equationEquation.get(x) == null) {
+        if (text == null || text.isEmpty()) {
+          continue;
+        } else {
+          equationEquation.set(x, new Equation(text, arrX, eqName));
+          equationEquation.get(x).setGlobalFunc(true);
+          continue;
+        }
+      }
+      if (text == null || text.isEmpty()) {
+        equationEquation.get(x).setGlobalFunc(false);
+        equationEquation.get(x).setName(null);
+        continue;
+      }
+      equationEquation.get(x).setEquation(text);
+      equationEquation.get(x).setName(eqName);
+      equationEquation.get(x).setGlobalFunc(true);
+    }
+  }
+  
+  /**
+   * Called after adding or removing an equation to update the GUI
+   */
+  private void rebuildTabEquation() {
+    // makes sure that panelTopEquation is ready
+    if (panelTopEquation == null) {
+      panelTopEquation = new JPanel();
+      panelTopEquation.setLayout(new GridLayout(0, 3));// 0 will scale # of rows as needed
+    } else {
+      panelTopEquation.removeAll();
+    }
+    
+    // adds components for equations to panelTopEquation
+    JButton tmpButton;
+    for (int x = 0; x < equationText.size(); x++) {
+      panelTopEquation.add(equationName.get(x));
+      panelTopEquation.add(equationText.get(x));
+      tmpButton = new JButton("Delete equation");
+      final int tmpVar = x;// TODO there has to be a better way...
+      tmpButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          removeEquation(tmpVar);
+        }
+      });
+      panelTopEquation.add(tmpButton);
+    }
+    
+    // adds the bottom row to panelTopEquation
+    // tmpButton = new JButton("Sort");
+    // tmpButton.addActionListener(new ActionListener() {
+    // @Override
+    // public void actionPerformed(ActionEvent e) {
+    // // TODO Auto-generated method stub
+    // JOptionPane.showMessageDialog(GraphFrame.this,
+    // "Sorting equations by name has not\nbeen implemented yet. Sorry!\n(Won't be implemented for a
+    // while;\nnot until you can rename equations)");
+    // }
+    // });
+    panelTopEquation.add(new JLabel());// srsly... Why can't I just do null.... :(
+    // panelTopEquation.add(tmpButton);
+    tmpButton = new JButton("add equation");
+    tmpButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        addEquation();
+      }
+    });
+    panelTopEquation.add(tmpButton);
+    tmpButton = new JButton("Graph!");
+    tmpButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        rebuildEquations();
+        
+        double xMin = textXMin.getValueDouble();
+        double xMax = textXMax.getValueDouble();
+        double yMin = textYMin.getValueDouble();
+        double yMax = textYMax.getValueDouble();
+        if ((xMin >= xMax) || (yMin >= yMax)) {
+          JOptionPane.showMessageDialog(null,
+            "The maximum window bound must be greater than the minimum", "Error",
+            JOptionPane.ERROR_MESSAGE);
+          return;
+        }
+        panelGraph.setXMin(xMin);
+        panelGraph.setXMax(xMax);
+        panelGraph.setYMin(yMin);
+        panelGraph.setYMax(yMax);
+        
+        panelGraph.repaint();
+      }
+    });
+    panelTopEquation.add(tmpButton);
+    pack();
+  }
+  
+  protected void removeEquation(int i) {
+    equationText.remove(i);
+    equationName.remove(i);
+    equationEquation.get(i).setName(null);
+    equationEquation.get(i).setGlobalFunc(false);
+    equationEquation.remove(i);
+    rebuildTabEquation();
+  }
+}
---
+++ com/silentsalamander/AGE/GraphPanel.java
@@ -0,0 +1,135 @@
+// This file is part of AGE
+//
+// AGE Graphs Equations (AGE) is a java program that graphs equations
+// Copyright (C) 2016 Ivan Johnson:
+// fire.4@cox.net
+//
+// AGE is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package com.silentsalamander.AGE;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.logging.Level;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import com.silentsalamander.helper.PrettyLogger;
+import com.silentsalamander.helper.equation.Equation;
+
+class GraphPanel extends JPanel {// only visible within this package
+  // This is all code I wrote years ago when I was much less experienced with java, so:
+  // TODO try to clean up this big mess of a class
+  
+  
+  protected static PrettyLogger log;
+  private static final long     serialVersionUID = 8666758096652663747L;
+  static {
+    log = PrettyLogger.getPrimaryLogger();
+  }
+  
+  private final Color  COLORBACKROUND = Color.LIGHT_GRAY;
+  
+  protected GraphFrame graphFrame;
+  
+  protected double     xMin, xMax, yMin, yMax;
+  
+  public GraphPanel(GraphFrame graphFrame) {
+    this.graphFrame = graphFrame;
+  }
+  
+  private void drawAxis(Graphics g) {
+    double yAxisxPercent = (0 - xMin) / (xMax - xMin);
+    
+    int originYCoord = valueToPixelY(0.0D);
+    int originXCoord = (int) (yAxisxPercent * getWidth());
+    
+    Color c = g.getColor();
+    g.setColor(Color.ORANGE);
+    
+    g.drawLine(0, originYCoord - 1, getWidth(), originYCoord - 1);
+    g.drawLine(0, originYCoord, getWidth(), originYCoord);
+    g.drawLine(0, originYCoord + 1, getWidth(), originYCoord + 1);
+    
+    g.drawLine(originXCoord - 1, valueToPixelY(yMin), originXCoord - 1, valueToPixelY(yMax));
+    g.drawLine(originXCoord, valueToPixelY(yMin), originXCoord, valueToPixelY(yMax));
+    g.drawLine(originXCoord + 1, valueToPixelY(yMin), originXCoord + 1, valueToPixelY(yMax));
+    
+    g.setColor(c);
+  }
+  
+  @Override
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    drawAxis(g);
+    
+    g.setColor(COLORBACKROUND);
+    g.fillRect(0, 0, getWidth(), getHeight());
+    g.setColor(Color.BLACK);
+    
+    drawAxis(g);
+    
+    Equation[] equations = graphFrame.getEquation();
+    if (equations == null || equations.length == 0) {
+      return;
+    }
+    
+    double lastY, thisY;
+    double[] arrX = { 0 };
+    for (Equation eq : equations) {
+      if (eq == null) {
+        continue;// this happens when there is an empty text field
+      }
+      arrX[0] = pixelToGraphX(-1);
+      lastY = eq.evaluate(arrX);
+      for (int x = 0; x <= getWidth() + 1; x++) {
+        try {
+          arrX[0] = pixelToGraphX(x);
+          thisY = eq.evaluate(arrX);
+        } catch (Exception e) {
+          JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+          log.log(Level.WARNING, "caught exeption during graphing... halting proccess.", e);
+          break;
+        }
+        g.drawLine(x - 1, valueToPixelY(lastY), x, valueToPixelY(thisY));
+        lastY = thisY;
+      }
+    }
+  }
+  
+  private double pixelToGraphX(int x) {
+    return (xMax - xMin) / getWidth() * x + xMin;
+  }
+  
+  public void setXMax(double xMax) {
+    this.xMax = xMax;
+  }
+  
+  public void setXMin(double xMin) {
+    this.xMin = xMin;
+  }
+  
+  public void setYMax(double yMax) {
+    this.yMax = yMax;
+  }
+  
+  public void setYMin(double yMin) {
+    this.yMin = yMin;
+  }
+  
+  private int valueToPixelY(double yValue) {
+    return (int) (getHeight() / (yMin - yMax) * (yValue - yMax));
+  }
+}
